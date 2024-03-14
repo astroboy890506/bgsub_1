f70bf0a7fedf187b92223da0e8416c976a630e18
astroboy890506 <97178858+astroboy890506@users.noreply.github.com>
2024-03-14 18:43:03 +0800

    Update backgroundsubstractor_1.py

--- backgroundsubstractor_1.py
+++ backgroundsubstractor_1.py
@@ -6,11 +6,31 @@ import tempfile
 
 # Function to return a background subtractor
 def get_bgsubtractor(BGS_TYPE):
+    # https://docs.opencv.org/3.4/d1/d5c/classcv_1_1bgsegm_1_1BackgroundSubtractorGMG.html
+    if BGS_TYPE == 'GMG':
+        return cv2.bgsegm.createBackgroundSubtractorGMG(initializationFrames = 120,
+                                                        decisionThreshold = 0.8)
+    # https://docs.opencv.org/3.4/d6/da7/classcv_1_1bgsegm_1_1BackgroundSubtractorMOG.html
+    if BGS_TYPE == 'MOG':
+        return cv2.bgsegm.createBackgroundSubtractorMOG(history = 200, nmixtures = 5,
+                                                       backgroundRatio = 0.7, noiseSigma=0)
+    # https://docs.opencv.org/3.4/d7/d7b/classcv_1_1BackgroundSubtractorMOG2.html
+    if BGS_TYPE == 'MOG2':
+        return cv2.createBackgroundSubtractorMOG2(history = 500, detectShadows = True,
+                                                  varThreshold = 100)
+    # https://docs.opencv.org/3.4/db/d88/classcv_1_1BackgroundSubtractorKNN.html
     if BGS_TYPE == 'KNN':
-        return cv2.createBackgroundSubtractorKNN(history=500, dist2Threshold=400, detectShadows=True)
+        return cv2.createBackgroundSubtractorKNN(history = 500, dist2Threshold=400,
+                                                 detectShadows = True)
+    # https://docs.opencv.org/3.4/de/dca/classcv_1_1bgsegm_1_1BackgroundSubtractorCNT.html
+    if BGS_TYPE == 'CNT':
+        return cv2.bgsegm.createBackgroundSubtractorCNT(minPixelStability=15,
+                                                        useHistory = True,
+                                                        maxPixelStability = 15*60,
+                                                        isParallel=True)
 
 st.title("Background Subtraction Demo")
-
+bg_subtractor_type = st.selectbox("Select Background Subtraction Method", ('GMG', 'MOG', 'MOG2', 'KNN', 'CNT'))
 uploaded_file = st.file_uploader("Choose a video...", type=["mp4", "avi", "mov", "mkv"])
 
 # Initialize session state
@@ -23,7 +43,7 @@ if uploaded_file is not None and not st.session_state.processing_started:
     tfile.write(uploaded_file.getvalue())
     cap = cv2.VideoCapture(tfile.name)
 
-    bg_subtractor = get_bgsubtractor('KNN')
+    bg_subtractor_type = get_bgsubtractor(bg_subtractor_type)
 
     stframe = st.empty()
 
